fb6a6ead7045141a033dbbe806fae946053f00a4
Michael Beckemeyer <m.beckemeyer@conterra.de>
2023-05-15 16:50:59 +0200

    setup new package (open-pioneer/starter#63)

---
+++ packages/build-package/src/index.ts
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: con terra GmbH and contributors
+// SPDX-License-Identifier: Apache-2.0
+import type { BuildOptions, BuildResult } from "../types";
+
+export async function build(_options: BuildOptions): Promise<BuildResult> {
+    throw new Error("Not implemented yet.");
+}
