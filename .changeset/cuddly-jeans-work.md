453ce3afae8e8f6e5bd97c0a0ffbae0595807ebd
Michael Beckemeyer <m.beckemeyer@conterra.de>
2024-07-23 14:31:16 +0200

    Update dependencies

---
+++ .changeset/cuddly-jeans-work.md
@@ -0,0 +1,8 @@
+---
+"@open-pioneer/tag-current-versions": patch
+"@open-pioneer/build-package": patch
+"@open-pioneer/build-common": patch
+"@open-pioneer/vite-plugin-pioneer": patch
+---
+
+Update dependencies
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 import { defineConfig } from "vitest/config";
 
+const IGNORED_FILES = ["**/dist/**", "**/test-data/**", "**/temp/**", "**/node_modules/**"];
+
 export default defineConfig({
     test: {
-        exclude: ["**/dist/**", "**/test-data/**", "**/temp/**", "**/node_modules/**"],
-        watchExclude: [
-            "**/node_modules/**", "**/dist/**", "**/temp/**",
-        ]
+        exclude: IGNORED_FILES,
+    },
+    server: {
+        watch: {
+            ignored: IGNORED_FILES
+        }
     }
 });
