7d3d9dd38112dda92a3e5b4a07b9f4bfc7226ca8
Arne Vogt <a.vogt@52north.org>
2024-12-05 09:01:46 +0100

    replace deprecated eslint rule

--- support/build.js
+++ support/build.js
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 import esbuild from "esbuild";
 
 /*
