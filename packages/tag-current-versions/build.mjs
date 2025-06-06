14ba97663515e9462fd92b37058bdb21e65382ea
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-06-06 11:55:20 +0200

    Remove obsolete package tag-current-versions

--- packages/tag-current-versions/build.mjs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
-// SPDX-License-Identifier: Apache-2.0
-import { build } from "build-tools/support/build.js";
-
-const mode = process.argv[2];
-build(mode).catch((e) => {
-    console.error(e);
-    process.exit(1);
-});
--- packages/tag-current-versions/src/index.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
-// SPDX-License-Identifier: Apache-2.0
-import { cwd, exit } from "node:process";
-import { run } from "./run";
-
-run(cwd()).catch((e) => {
-    console.error("Fatal error", e);
-    exit(1);
-});
