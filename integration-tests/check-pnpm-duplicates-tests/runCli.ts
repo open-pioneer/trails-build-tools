cd2b28901b1a85ea865ad0978047a9b92fa4baa3
Antonia van Eek <a.vaneek@conterra.de>
2025-08-14 15:37:23 +0200

    fix integration tests on windows

--- integration-tests/check-pnpm-duplicates-tests/runCli.ts
+++ integration-tests/check-pnpm-duplicates-tests/runCli.ts
@@ -1,8 +1,13 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 import { resolve } from "node:path";
-import { $, ProcessOutput } from "zx";
+import { $, ProcessOutput, usePowerShell } from "zx";
 import { PACKAGE_DIR } from "./paths";
+
+const os = process.platform; //returns win32 even on win 64bit
+if (os === "win32") {
+    usePowerShell();
+}
 
 export interface RunResult {
     exitCode: number;
@@ -10,14 +15,17 @@ export interface RunResult {
 }
 
 export async function runCli(workdir: string, configPath: string | undefined): Promise<RunResult> {
-    const cli = resolve(PACKAGE_DIR, "node_modules/.bin/check-pnpm-duplicates");
+    const cli = resolve(
+        PACKAGE_DIR,
+        "node_modules/@open-pioneer/check-pnpm-duplicates/dist/index.js"
+    );
     const flags: string[] = ["--debug"];
     if (configPath) {
         flags.push("-c", configPath);
     }
 
     try {
-        const result = await $({ cwd: workdir })`${cli} ${flags}`.quiet();
+        const result = await $({ cwd: workdir })`node ${cli} ${flags}`.quiet();
         return { exitCode: 0, output: result.toString() };
     } catch (e) {
         if (e instanceof ProcessOutput) {
