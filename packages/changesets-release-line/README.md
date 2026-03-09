3e49faae8125fb7c523dca88a759d8a223dd73ae
Arne <a.vogt@52north.org>
2026-03-09 10:43:45 +0100

    add package for custom changesets release line

---
+++ packages/changesets-release-line/README.md
@@ -0,0 +1,3 @@
+# @open-pioneer/changelogs-release-line
+
+This package provides a custom release line for `@changesets/cli` to be used in official Open Pioneer Trails repositories.
---
+++ packages/changesets-release-line/src/index.ts
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+    NewChangesetWithCommit,
+    VersionType,
+    ChangelogFunctions,
+    ModCompWithPackage
+} from "@changesets/types";
+
+const getReleaseLine = async (changeset: NewChangesetWithCommit, _type: VersionType) => {
+    const [firstLine, ...futureLines] = changeset.summary.split("\n").map((l) => l.trimEnd());
+
+    let returnVal = `- ${changeset.commit ? `${changeset.commit.slice(0, 7)}: ` : ""}${firstLine}`;
+
+    if (futureLines.length > 0) {
+        returnVal += `\n${futureLines.map((l) => `  ${l}`).join("\n")}`;
+    }
+
+    return returnVal;
+};
+
+const getDependencyReleaseLine = async (
+    _changesets: NewChangesetWithCommit[],
+    _dependenciesUpdated: ModCompWithPackage[]
+) => {
+    //do not log internal dependency updates in changelogs
+    return "";
+};
+
+const changelogFunctions: ChangelogFunctions = {
+    getReleaseLine,
+    getDependencyReleaseLine
+};
+
+export default changelogFunctions;
