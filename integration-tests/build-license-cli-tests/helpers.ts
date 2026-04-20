a275c0f5943f813c1d8d6d67b7f340dac009a0ca
a.marks <a.marks@conterra.de>
2026-04-20 12:07:32 +0200

    replace package.json from tests

--- integration-tests/build-license-cli-tests/helpers.ts
+++ integration-tests/build-license-cli-tests/helpers.ts
@@ -8,6 +8,11 @@ import { PROJECT_DIR, TEMP_PATH } from "./paths";
 export function useTemporaryPnpmLockfile(): void {
     beforeAll(() => {
         cpSync(PROJECT_DIR, TEMP_PATH, { recursive: true, force: true });
+
+        const packageJsonSrc = resolve(TEMP_PATH, "_package.json");
+        const packageJsonDest = resolve(TEMP_PATH, "package.json");
+        cpSync(packageJsonSrc, packageJsonDest, { recursive: true, force: true });
+
         const lockFileSrc = resolve(TEMP_PATH, "_pnpm-lock.yaml");
         const lockFileDest = resolve(TEMP_PATH, "pnpm-lock.yaml");
         cpSync(lockFileSrc, lockFileDest, { recursive: true, force: true });
