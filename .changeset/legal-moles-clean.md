7d706a34d056d743441a49fcaf6d1937e8898a22
Michael Beckemeyer <m.beckemeyer@conterra.de>
2026-06-25 14:35:42 +0200

    Use private javascript properties instead of typescript keyword (#182)

---
+++ .changeset/legal-moles-clean.md
@@ -0,0 +1,6 @@
+---
+"@open-pioneer/build-package": patch
+"@open-pioneer/vite-plugin-pioneer": patch
+---
+
+Refactor: use private javascript properties instead of typescript private keyword
--- packages/vite-plugin/src/codegen/IdGenerator.ts
+++ packages/vite-plugin/src/codegen/IdGenerator.ts
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
 export class IdGenerator {
-    private existing = new Map<string, number>();
+    #existing = new Map<string, number>();
 
     /**
      * Returns a unique identifier (in the context of this instance) that resembles the input value.
@@ -9,7 +9,7 @@ export class IdGenerator {
     generate(input: string): string {
         const clean = cleanName(input);
 
-        const existing = this.existing;
+        const existing = this.#existing;
         const count = existing.get(clean) ?? 0;
         existing.set(clean, count + 1);
         return count === 0 ? clean : `${clean}_${count}`;
